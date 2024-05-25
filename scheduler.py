fe20f632e26cd3716cfbe7d7447427869e1d2be7
Aleksei Romanov <54277470+saveasguy@users.noreply.github.com>
2024-05-26 01:13:42 +0300

    Project & board choice view implemented (#9)

--- scheduler.py
+++ scheduler.py
@@ -1,3 +1,5 @@
+from customtkinter import CTkFrame
+
 from scheduler import controllers
 from scheduler import models
 from scheduler import views
@@ -7,16 +9,32 @@ class App(controllers.IApp):
     def __init__(self):
         super().__init__()
         self.title("Smart Scheduler")
-        self.geometry("300x300")
+        self.geometry("500x500")
 
+        self.frame: CTkFrame | None = None
         self.model = models.AppLogicModel()
-        self.login_view = views.LoginView(
-            self, controllers.LoginController(self)
-        )
-        self.login_view.pack()
+        self.views = {}
+        self.controllers = {}
+
+        self.views["login"] = views.LoginView
+        self.controllers["login"] = controllers.LoginController(self)
+
+        self.views["boards"] = views.BoardView
+        self.controllers["boards"] = controllers.BoardController(self)
+
+        self.show_view("login")
 
     def get_model(self) -> models.AppLogicModel:
         return self.model
+
+    def show_view(self, id: str):
+        view_cls = self.views[id]
+        controller = self.controllers[id]
+        if self.frame:
+            self.frame.pack_forget()
+            self.frame.destroy()
+        self.frame = view_cls(self, controller)
+        self.frame.pack(fill="both", expand=True)
 
 
 def main():
--- scheduler/controllers.py
+++ scheduler/controllers.py
@@ -1,14 +1,24 @@
-import tkinter as tk
+from customtkinter import CTk
+from itertools import chain
 
 from scheduler import models
 from scheduler import views
+from typing import List
 
 
-class IApp(tk.Tk):
+class IApp(CTk):
     def __init__(self):
         super().__init__()
 
     def get_model(self) -> models.AppLogicModel:
+        raise NotImplementedError()
+
+    def show_view(self, id: str):
+        """Show the view with the given id
+
+        :param id: Id of the view
+        :type id: str
+        """
         raise NotImplementedError()
 
 
@@ -17,6 +27,13 @@ class LoginController(views.ILoginController):
         self.app = app
 
     def on_auth(self, view: views.LoginView):
+        """Callback which is invoked when login button is pressed.
+        If login failed, call views method to display authorization failure.
+        Otherwise show page allowing to choose project and board
+
+        :param view: Login page, which should call this method.
+        :type view: views.LoginView
+        """
         try:
             self.app.get_model().auth(
                 view.user.get(),
@@ -26,4 +43,41 @@ class LoginController(views.ILoginController):
         except ValueError:
             view.handle_authorization_error()
             return
-        view.error.set("Success!")
+        self.app.show_view("boards")
+
+
+class BoardController(views.IBoardController):
+    def __init__(self, app: IApp):
+        self.app = app
+        self.projects: List[models.Project] = []
+        self.boards: List[models.Board] = []
+
+    def get_project_names(self) -> List[str]:
+        self.projects = self.app.get_model().get_projects()
+        return [prj.title for prj in self.projects]
+
+    def get_board_names_by_project_name(self, project_name: str) -> List[str]:
+        found_projects = [
+            prj for prj in self.projects if prj.title == project_name
+        ]
+        if len(found_projects) != 1:
+            raise RuntimeError(
+                "Zero projects found or unexpected projects with the same name"
+            )
+        self.boards = self.app.get_model().get_boards_by_project(
+            found_projects[0]
+        )
+        return [board.title for board in self.boards]
+
+    def on_choose_board(self, board_name: str):
+        found_boards = [
+            board for board in self.boards if board.title == board_name
+        ]
+        if len(found_boards) != 1:
+            raise RuntimeError(
+                "Zero boards found or unexpected boards with the same name"
+            )
+        all_tasks = chain.from_iterable(
+            self.app.get_model().get_tasks_by_board(found_boards[0])
+        )
+        print("\n".join(task.title for task in all_tasks))
